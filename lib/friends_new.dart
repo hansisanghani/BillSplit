99b8d7238e5aef64f4ba7f6054ee26a4ea1b5aa9
Parth010114 <73269416+Parth010114@users.noreply.github.com>
2020-11-29 18:57:26 +0530

    Add files via upload

---
+++ lib/friends_new.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'appbar.dart';
+
+class FriendsNew extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                MyAppBar(
+                  title: Text(
+                    'FRIENDS',
+                    style: Theme.of(context).primaryTextTheme.headline6,
+                  ),
+                ),
+                Container(
+                  child: Text('Friend',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.tealAccent,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  padding: EdgeInsets.all(20.0),
+                ),
+                Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Name", border: OutlineInputBorder()),
+                      keyboardType: TextInputType.name,
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Amount", border: OutlineInputBorder()),
+                      keyboardType: TextInputType.name,
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Description",
+                          border: OutlineInputBorder()),
+                      keyboardType: TextInputType.name,
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+              ]),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.tealAccent,
+      ),
+    );
+  }
+}
